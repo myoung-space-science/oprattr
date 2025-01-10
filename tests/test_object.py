90112078f4415714da35e8477f7e568f78bb86e9
Matthew Young <myoung.space.science@gmail.com>
2025-01-09 21:35:04 -0500

    Redefine `pow` logic and update tests
    * Create Symbol class
    * Remove Name and Spin classes
    * Rewrite tests using Symbol instances for test
    attributes
    * Redefine test_exponential to reflect new
    behavior for exponential operations

--- tests/test_object.py
+++ tests/test_object.py
@@ -1,7 +1,100 @@
+import numbers
+
 import numpy
 import pytest
 
 import attrops
+
+
+class Symbol:
+    """A symbolic test attribute."""
+
+    def __init__(self, __x: str):
+        self._x = __x
+
+    def __repr__(self):
+        return f"{self.__class__.__qualname__}({self})"
+
+    def __str__(self):
+        return self._x
+
+    def __abs__(self):
+        return f"abs({self._x})"
+
+    def __pos__(self):
+        return f"+{self._x}"
+
+    def __neg__(self):
+        return f"-{self._x}"
+
+    def __eq__(self, other):
+        if isinstance(other, Symbol):
+            return self._x == other._x
+        if isinstance(other, str):
+            return self._x == other
+        return False
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __le__(self, other):
+        return symbolic_binary(self, '<=', other)
+
+    def __lt__(self, other):
+        return symbolic_binary(self, '<', other)
+
+    def __ge__(self, other):
+        return symbolic_binary(self, '>=', other)
+
+    def __gt__(self, other):
+        return symbolic_binary(self, '>', other)
+
+    def __add__(self, other):
+        return symbolic_binary(self, '+', other)
+
+    def __radd__(self, other):
+        return symbolic_binary(other, '+', self)
+
+    def __sub__(self, other):
+        return symbolic_binary(self, '-', other)
+
+    def __rsub__(self, other):
+        return symbolic_binary(other, '-', self)
+
+    def __mul__(self, other):
+        return symbolic_binary(self, '*', other)
+
+    def __rmul__(self, other):
+        return symbolic_binary(other, '-', self)
+
+    def __truediv__(self, other):
+        return symbolic_binary(self, '/', other)
+
+    def __rtruediv__(self, other):
+        return symbolic_binary(other, '/', self)
+
+    def __floordiv__(self, other):
+        return symbolic_binary(self, '//', other)
+
+    def __rfloordiv__(self, other):
+        return symbolic_binary(other, '//', self)
+
+    def __mod__(self, other):
+        return symbolic_binary(self, '%', other)
+
+    def __rmod__(self, other):
+        return symbolic_binary(other, '%', self)
+
+    def __pow__(self, other):
+        if isinstance(other, numbers.Real):
+            return f"{self} ** {other}"
+        return NotImplemented
+
+
+def symbolic_binary(a, op, b):
+    if isinstance(a, (Symbol, str)) and isinstance(b, (Symbol, str)):
+        return f"{a} {op} {b}"
+    return NotImplemented
 
 
 def test_initialize():
@@ -32,10 +125,10 @@ def test_equality():
     assert x(1) != x(-1)
     assert x(1) == 1
     assert x(1) != -1
-    assert x(1, name='A') == x(1, name='A')
-    assert x(1, name='A') != x(1, name='B')
-    assert x(1, name='A') != 1
-    assert 1 != x(1, name='A')
+    assert x(1, name=Symbol('A')) == x(1, name=Symbol('A'))
+    assert x(1, name=Symbol('A')) != x(1, name=Symbol('B'))
+    assert x(1, name=Symbol('A')) != 1
+    assert 1 != x(1, name=Symbol('A'))
 
 
 def test_ordering():
@@ -46,24 +139,24 @@ def test_ordering():
     assert x(1) > x(0)
     assert x(1) >= x(0)
     assert x(1) >= x(1)
-    assert x(1, name='A') < x(2, name='A')
-    assert x(1, name='A') <= x(2, name='A')
-    assert x(1, name='A') <= x(1, name='A')
-    assert x(1, name='A') > x(0, name='A')
-    assert x(1, name='A') >= x(0, name='A')
-    assert x(1, name='A') >= x(1, name='A')
+    assert x(1, name=Symbol('A')) < x(2, name=Symbol('A'))
+    assert x(1, name=Symbol('A')) <= x(2, name=Symbol('A'))
+    assert x(1, name=Symbol('A')) <= x(1, name=Symbol('A'))
+    assert x(1, name=Symbol('A')) > x(0, name=Symbol('A'))
+    assert x(1, name=Symbol('A')) >= x(0, name=Symbol('A'))
+    assert x(1, name=Symbol('A')) >= x(1, name=Symbol('A'))
     with pytest.raises(TypeError):
-         x(1, name='A') < x(2, name='B')
+         x(1, name=Symbol('A')) < x(2, name=Symbol('B'))
     with pytest.raises(TypeError):
-         x(1, name='A') <= x(2, name='B')
+         x(1, name=Symbol('A')) <= x(2, name=Symbol('B'))
     with pytest.raises(TypeError):
-         x(1, name='A') <= x(1, name='B')
+         x(1, name=Symbol('A')) <= x(1, name=Symbol('B'))
     with pytest.raises(TypeError):
-         x(1, name='A') > x(0, name='B')
+         x(1, name=Symbol('A')) > x(0, name=Symbol('B'))
     with pytest.raises(TypeError):
-         x(1, name='A') >= x(0, name='B')
+         x(1, name=Symbol('A')) >= x(0, name=Symbol('B'))
     with pytest.raises(TypeError):
-         x(1, name='A') >= x(1, name='B')
+         x(1, name=Symbol('A')) >= x(1, name=Symbol('B'))
     assert x(1) < +2
     assert x(1) <= +2
     assert x(1) <= +1
@@ -71,34 +164,17 @@ def test_ordering():
     assert x(1) >= 0
     assert x(1) >= +1
     with pytest.raises(TypeError):
-         x(1, name='A') < +2
+         x(1, name=Symbol('A')) < +2
     with pytest.raises(TypeError):
-         x(1, name='A') <= +2
+         x(1, name=Symbol('A')) <= +2
     with pytest.raises(TypeError):
-         x(1, name='A') <= +1
+         x(1, name=Symbol('A')) <= +1
     with pytest.raises(TypeError):
-         x(1, name='A') > 0
+         x(1, name=Symbol('A')) > 0
     with pytest.raises(TypeError):
-         x(1, name='A') >= 0
+         x(1, name=Symbol('A')) >= 0
     with pytest.raises(TypeError):
-         x(1, name='A') >= +1
-
-
-class Spin:
-    def __init__(self, __v: int):
-        self._v = __v
-    def __repr__(self):
-        return f"Spin({self._v!r})"
-    def __eq__(self, other):
-        if isinstance(other, Spin):
-            return self._v == other._v
-        if isinstance(other, (int, float)):
-            return self._v == other
-        return False
-    def __abs__(self):
-        return abs(self._v)
-    def __neg__(self):
-        return -self._v
+         x(1, name=Symbol('A')) >= +1
 
 
 def test_unary():
@@ -106,80 +182,51 @@ def test_unary():
     assert abs(x(-1)) == x(1)
     assert +x(-1) == x(-1)
     assert -x(1) == x(-1)
-    assert abs(x(-1, spin=Spin(-2))) == x(1, spin=Spin(2))
+    assert abs(x(-1, name=Symbol('A'))) == x(1, name=Symbol('abs(A)'))
+    assert +x(1, name=Symbol('A')) == x(+1, name=Symbol('+A'))
+    assert -x(1, name=Symbol('A')) == x(-1, name=Symbol('-A'))
     with pytest.raises(TypeError):
-        +x(-1, spin=Spin(1))
-    assert -x(1, spin=Spin(2)) == x(-1, spin=Spin(-2))
+        abs(x(-1, name='A'))
+    with pytest.raises(TypeError):
+        +x(1, name='A')
+    with pytest.raises(TypeError):
+        -x(1, name='A')
 
 
 def test_additive():
     """Test the + and - operations."""
     assert x(1) + x(2) == x(3)
-    assert x(1, name='A') + x(2, name='A') == x(3, name='A')
+    nA = Symbol('A')
+    nB = Symbol('B')
+    assert x(1, name=nA) + x(2, name=nA) == x(3, name=nA)
     with pytest.raises(TypeError):
-        x(1, name='A') + x(2, name='B')
+        x(1, name=nA) + x(2, name=nB)
     assert x(1) - x(2) == x(-1)
-    assert x(1, name='A') - x(2, name='A') == x(-1, name='A')
+    assert x(1, name=nA) - x(2, name=nA) == x(-1, name=nA)
     with pytest.raises(TypeError):
-        x(1, name='A') - x(2, name='B')
+        x(1, name=nA) - x(2, name=nB)
     assert x(1) + 2 == x(3)
     with pytest.raises(TypeError):
-        x(1, name='A') + 2
+        x(1, name=nA) + 2
     assert x(1) - 2 == x(-1)
     with pytest.raises(TypeError):
-        x(1, name='A') - 2
+        x(1, name=nA) - 2
     assert 2 + x(1) == x(3)
     with pytest.raises(TypeError):
-        2 + x(1, name='A')
+        2 + x(1, name=nA)
     assert 2 - x(1) == x(1)
     with pytest.raises(TypeError):
-        2 - x(1, name='A')
-
-
-class Name:
-    def __init__(self, __v: str):
-        self._v = __v
-    def __repr__(self):
-        return f"Name({self._v!r})"
-    def __eq__(self, other):
-        if isinstance(other, Name):
-            return self._v == other._v
-        if isinstance(other, str):
-            return self._v == other
-        return False
-    def __mul__(self, other):
-        if isinstance(other, (Name, str)):
-            return Name(f"{self._v}{other._v}")
-        return NotImplemented
-    def __truediv__(self, other):
-        if isinstance(other, (Name, str)):
-            return Name(f"{self._v}{other._v}")
-        return NotImplemented
-    def __floordiv__(self, other):
-        if isinstance(other, (Name, str)):
-            return Name(f"{self._v}{other._v}")
-        return NotImplemented
-    def __mod__(self, other):
-        if isinstance(other, (Name, str)):
-            return Name(f"{self._v}{other._v}")
-        return NotImplemented
-    def __pow__(self, other):
-        if isinstance(other, (Name, str)):
-            return Name(f"{self._v}^{other._v}")
-        if isinstance(other, (float, int)):
-            return Name(f"{self._v * other}")
-        return NotImplemented
+        2 - x(1, name=nA)
 
 
 def test_multiplicative():
     """Test the *, /, //, and % operations."""
-    nA = Name('A')
-    nB = Name('B')
-    nAB = Name('AB')
-    assert x(3, name=nA) * x(2, name=nB) == x(6, name=nAB)
-    assert x(3, name=nA) / x(2, name=nB) == x(1.5, name=nAB)
-    assert x(3, name=nA) // x(2, name=nB) == x(1, name=nAB)
-    assert x(6, name=nA) % x(2, name=nB) == x(0, name=nAB)
+    nA = Symbol('A')
+    nB = Symbol('B')
+    assert x(3, name=nA) * x(2, name=nB) == x(6, name=Symbol('A * B'))
+    assert x(3, name=nA) / x(2, name=nB) == x(1.5, name=Symbol('A / B'))
+    assert x(3, name=nA) // x(2, name=nB) == x(1, name=Symbol('A // B'))
+    assert x(6, name=nA) % x(2, name=nB) == x(0, name=Symbol('A % B'))
     assert x(3, name=nA) * x(2) == x(6, name=nA)
     assert x(3, name=nA) / x(2) == x(1.5, name=nA)
     assert x(3, name=nA) // x(2) == x(1, name=nA)
@@ -216,13 +263,23 @@ def test_multiplicative():
 
 def test_exponential():
     """Test the ** operation."""
-    assert x(3) ** x(2) == x(9)
     assert x(3) ** 2 == x(9)
-    assert 3 ** x(2) == x(9)
-    nA = Name('A')
-    assert x(3, name=nA) ** x(2) == x(9, name=nA)
-    assert x(3, name=nA) ** x(2, name=Name('B')) == x(9, name=Name('A^B'))
-    assert x(3, name=nA) ** 2 == x(9, name=Name('AA'))
+    with pytest.raises(TypeError):
+        x(3) ** x(2)
+    with pytest.raises(TypeError):
+        3 ** x(2)
+    nA = Symbol('A')
+    assert x(3, name=nA) ** 2 == x(9, name=Symbol('A ** 2'))
+    with pytest.raises(TypeError):
+        x(3, name=nA) ** x(2, name='B')
+    with pytest.raises(TypeError):
+        x(3, name='A') ** 2
+    with pytest.raises(TypeError):
+        x(3, name='A') ** x(2, name='A')
+    with pytest.raises(TypeError):
+        x(3, name=nA) ** x(2)
     with pytest.raises(TypeError):
         3 ** x(2, name=nA)
+    with pytest.raises(TypeError):
+        x(3) ** x(2, name=nA)
 
